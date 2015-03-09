afe65d296e92ae71896013652f0261087e784b0f
Aurelien Tran <aurelien.tran@gmail.com>
2015-03-09 15:56:54 +0900

    Move Cpu type definition to header file.

--- src/Cpu.c
+++ src/Cpu.c
@@ -43,18 +43,6 @@
 #define CPU_NULL    0
 
 /**
- * Get 16 bit register pointer
- * @param reg The 16 bit register Cpu_RegName_e id
- */
-#define CPU_REG16(reg)  (&Cpu_Info.Reg[reg])
-
-/**
- * Get 8 bit register pointer
- * @param reg The 8 bit register Cpu_RegName_e id
- */
-#define CPU_REG8(reg)   (&Cpu_Info.Reg[(reg) / 2].Byte[(reg) % 2])
-
-/**
  * Concat 2 Byte data to 1 Word
  * @param data0 The first Byte
  * @param data1 The second Byte
@@ -85,79 +73,24 @@
 /* Type                                               */
 /******************************************************/
 
-/** All purpose 8 bit register type */
-typedef union tagCpu_Reg8_t
-{
-    uint8_t UByte;  /**< Unsigned 8 bit */
-    int8_t  SByte;  /**< Signed 8 bit */
-} Cpu_Reg8_t;
-
-/** All purpose 16 bit register type */
-typedef union tagCpu_Reg16_t
-{
-    uint16_t   UWord;     /**< Unsigned 16 bit access */
-    int16_t    SWord;     /**< Signed 16 bit access */
-    Cpu_Reg8_t Byte[2];   /**< 8 bit access */
-} Cpu_Reg16_t;
-
-/** CPU Register name */
-typedef enum tagCpu_RegName_e
-{
-    /* 16 bit Register Name */
-    CPU_R_AF = 0,   /**< Accumulator and Flag register */
-    CPU_R_BC,       /**< All purpose register */
-    CPU_R_DE,       /**< All purpose register */
-    CPU_R_HL,       /**< All purpose register */
-    CPU_R_SP,       /**< Stack Pointer register */
-    CPU_R_PC,       /**< Program Pointer register */
-    CPU_REG_NUM,    /**< Number of internal register */
-
-    /* 8 bit Register Name (little endian) */
-    CPU_R_F = 0,    /**< Flag register */
-    CPU_R_A,        /**< Accumulator register */
-    CPU_R_C,        /**< All purpose register */
-    CPU_R_B,        /**< All purpose register */
-    CPU_R_E,        /**< All purpose register */
-    CPU_R_D,        /**< All purpose register */
-    CPU_R_L,        /**< All purpose register */
-    CPU_R_H         /**< All purpose register */
-} Cpu_RegName_e;
-
-/** CPU Flag name */
-typedef enum tagCpu_FlagName_e
-{
-    CPU_F_Z   = 0x80,   /**< Zero Flag */
-    CPU_F_N   = 0x40,   /**< Substract Flag */
-    CPU_F_H   = 0x20,   /**< Half Carry Flag */
-    CPU_F_C   = 0x10,   /**< Carry Flag */
-    CPU_F_NO  = 0x00,   /**< No flag */
-    CPU_F_ALL = 0xF0    /**< All flag */
-} Cpu_FlagName_e;
-
-/** CPU Info */
-typedef struct tagCpu_Info_t
-{
-    Cpu_Reg16_t Reg[CPU_REG_NUM];   /**< Internal Register */
-} Cpu_Info_t;
-
 /* Forward declaration for Cpu_Callback_t definition */
 typedef struct tagCpu_OpCode_t Cpu_OpCode_t;
 
 /**
  * Callback to Execute an OpCode
- * @param dst Destination register
- * @param src Source register
+ * @param opcode The opcode to execute
+ * @return The number of cycle used to execute the opcode
  */
 typedef int (*Cpu_Callback_t)(Cpu_OpCode_t const * const opcode);
 
 /** CPU OpCode information */
 typedef struct tagCpu_OpCode_t
 {
-    uint32_t Value;         /**< OpCode Value */
-    uint32_t Size;          /**< OpCode Byte size */
-    char const * Name;             /**< OpCode Name */
-    uint32_t Param0;        /**< OpCode Param 1 */
-    uint32_t Param1;        /**< Opcode Param 2 */
+    uint32_t Value;     /**< OpCode Value */
+    uint32_t Size;      /**< OpCode Byte size */
+    char const * Name;  /**< OpCode Name */
+    uint32_t Param0;    /**< OpCode Param 1 */
+    uint32_t Param1;    /**< Opcode Param 2 */
     Cpu_Callback_t Callback; /**< OpCode execution callback */
 } Cpu_OpCode_t;
 
@@ -753,7 +686,7 @@ void Cpu_Initialize(void)
 }
 
 
-int Cpu_Step(void)
+uint32_t Cpu_Step(void)
 {
     /* Get instruction */
     uint8_t const data = Cpu_ReadPc();
--- src/Cpu.h
+++ src/Cpu.h
@@ -36,10 +36,77 @@
 /* Macro                                              */
 /******************************************************/
 
+/**
+ * Get CPU internal 16 bit register pointer
+ * @param reg The 16 bit register Cpu_RegName_e id
+ */
+#define CPU_REG16(reg)  (&Cpu_Info.Reg[reg])
+
+/**
+ * Get CPU internal 8 bit register pointer
+ * @param reg The 8 bit register Cpu_RegName_e id
+ */
+#define CPU_REG8(reg)   (&Cpu_Info.Reg[(reg) / 2].Byte[(reg) % 2])
+
 
 /******************************************************/
 /* Type                                               */
 /******************************************************/
+
+/** CPU Register name */
+typedef enum tagCpu_RegName_e
+{
+    /* 16 bit Register Name */
+    CPU_R_AF = 0,   /**< Accumulator and Flag register */
+    CPU_R_BC,       /**< All purpose register */
+    CPU_R_DE,       /**< All purpose register */
+    CPU_R_HL,       /**< All purpose register */
+    CPU_R_SP,       /**< Stack Pointer register */
+    CPU_R_PC,       /**< Program Pointer register */
+    CPU_REG_NUM,    /**< Number of internal register */
+
+    /* 8 bit Register Name (little endian) */
+    CPU_R_F = 0,    /**< Flag register */
+    CPU_R_A,        /**< Accumulator register */
+    CPU_R_C,        /**< All purpose register */
+    CPU_R_B,        /**< All purpose register */
+    CPU_R_E,        /**< All purpose register */
+    CPU_R_D,        /**< All purpose register */
+    CPU_R_L,        /**< All purpose register */
+    CPU_R_H         /**< All purpose register */
+} Cpu_RegName_e;
+
+/** CPU Flag name */
+typedef enum tagCpu_FlagName_e
+{
+    CPU_F_Z   = 0x80,   /**< Zero Flag bitmap */
+    CPU_F_N   = 0x40,   /**< Substract Flag bitmap */
+    CPU_F_H   = 0x20,   /**< Half Carry Flag bitmap */
+    CPU_F_C   = 0x10,   /**< Carry Flag bitmap */
+    CPU_F_NO  = 0x00,   /**< No flag bitmap */
+    CPU_F_ALL = 0xF0    /**< All flag (Zero, Substract, Half Carry, Carry) bitmap */
+} Cpu_FlagName_e;
+
+/** All purpose 8 bit register type */
+typedef union tagCpu_Reg8_t
+{
+    uint8_t UByte;  /**< Unsigned 8 bit */
+    int8_t  SByte;  /**< Signed 8 bit */
+} Cpu_Reg8_t;
+
+/** All purpose 16 bit register type */
+typedef union tagCpu_Reg16_t
+{
+    uint16_t   UWord;     /**< Unsigned 16 bit access */
+    int16_t    SWord;     /**< Signed 16 bit access */
+    Cpu_Reg8_t Byte[2];   /**< 8 bit access */
+} Cpu_Reg16_t;
+
+/** CPU Info */
+typedef struct tagCpu_Info_t
+{
+    Cpu_Reg16_t Reg[CPU_REG_NUM];   /**< Internal Register */
+} Cpu_Info_t;
 
 
 /******************************************************/
@@ -55,7 +122,7 @@ extern void Cpu_Initialize(void);
  * Process 1 CPU instruction
  * @return The number of cycle used for the instruction
  */
-extern int Cpu_Step(void);
+extern uint32_t Cpu_Step(void);
 
 /**
  * Print CPU internal state for debug purpose
